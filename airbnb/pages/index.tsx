c5b2cf70066ea3502a9d13c356e2ef6e7855b80a
Ijjaz Khan <ijazkhan19932@gmail.com>
2023-02-03 22:41:39 +0500

    airbnb
    airbnb

--- airbnb/pages/index.tsx
+++ airbnb/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header';
-
+import Banner from '../components/Banner'
 
 const Home: NextPage = () => {
   return (
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
       </Head>
       <Header/>
       { /* Banner */}
-      { /* Header will goes here */}
+      <Banner/>
     </div>
   )
 }
