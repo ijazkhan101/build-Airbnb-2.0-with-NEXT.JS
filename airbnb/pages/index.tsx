9b98ae3cb537fd117e85a5ca04f3973256ee392c
Ijjaz Khan <ijazkhan19932@gmail.com>
2023-02-03 15:26:21 +0500

    Airbnb
    Airbnb

---
+++ airbnb/pages/index.tsx
@@ -0,0 +1,28 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+        <h1 className="text-6xl font-bold">
+          Welcome to Next js
+        </h1>
+
+      <h1>lets build Airbnb</h1>
+      
+
+    
+      </main>
+
+    </div>
+  )
+}
+
+export default Home
