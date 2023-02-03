33be63717b141fc8553f5d5156c0f76f2b090b68
Ijjaz Khan <ijazkhan19932@gmail.com>
2023-02-03 15:50:06 +0500

    header
    header

---
+++ airbnb/components/Header.js
@@ -0,0 +1,10 @@
+
+function Header(){
+    return(
+        <div>
+            <h1>this is header</h1>
+        </div>
+    )
+}
+
+export default Header
--- airbnb/pages/index.tsx
+++ airbnb/pages/index.tsx
@@ -1,16 +1,18 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header';
+
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
         <title>lets build Airbnb 2023</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      { /* Header will goes here */}
-      { /* Header will goes here */}
+      <Header/>
+      { /* Banner */}
       { /* Header will goes here */}
     </div>
   )
